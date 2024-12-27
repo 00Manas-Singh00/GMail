863456559e5d0aa1c47493236342445bca0d836e
00Manas-Singh00 <110744893+00Manas-Singh00@users.noreply.github.com>
2024-12-27 22:08:54 +0530

    changes made in navigation bar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -5,10 +5,19 @@ const NavBar = () => {
     <div className="flex items-center justify-between mx-3 h-16">
       <div className="flex items-center gap-10">
         <div className="flex items-center gap-2">
-          <div>
+          <div className="p-3 text-xl rounded-full text-gray-950 hover:bg-gray-200 cursor-pointer">
             <RxHamburgerMenu />
           </div>
+          <img
+            className="w-8"
+            src="https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_256px.png"
+            alt="logo"
+          />
+          <h1 className="text-xl text-gray-500 text-bolder">GMail</h1>
         </div>
+      </div>
+      <div>
+        <input type="text" placeholder="Search Mail" />
       </div>
     </div>
   );
