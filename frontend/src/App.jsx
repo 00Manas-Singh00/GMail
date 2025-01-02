51ffcb5e97030221f27af9756226c67140210354
00Manas-Singh00 <110744893+00Manas-Singh00@users.noreply.github.com>
2025-01-03 00:43:07 +0530

    inbox created with mail template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,34 @@
 // import { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import Inbox from "./components/Inbox";
 import NavBar from "./components/NavBar";
-import SideBar from "./components/SideBar";
+// import SideBar from "./components/SideBar";
+import Body from "./components/Body";
+import Mail from "./components/Mail";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <Inbox />,
+      },
+      {
+        path: "/mail/:id",
+        element: <Mail />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className="h-screen bg-[#F6F8FC]">
       <NavBar />
-      <div className="p-2 flex">
-        <SideBar />
-        <Inbox />
-      </div>
+      <RouterProvider router={appRouter} />
     </div>
   );
 }
