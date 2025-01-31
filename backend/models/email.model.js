54e3334c3dcb5324a55b16048c4001ef6e99998c
00Manas-Singh00 <110744893+00Manas-Singh00@users.noreply.github.com>
2025-01-31 23:18:38 +0530

    changes made

--- backend/models/email.model.js
+++ backend/models/email.model.js
@@ -13,6 +13,10 @@ const emailSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref:'User'
+    },
   },
   { timestamps: true }
 );
